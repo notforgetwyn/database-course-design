4e38eb4f5231dd4c020f219700db26e07dd93a53
SHI HUAYUE <1315126403@qq.com>
2024-06-19 11:18:10 +0800

    test mybatis

--- SpringBoot/src/test/java/com/java/springboot/ApplicationTests.java
+++ SpringBoot/src/test/java/com/java/springboot/ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.java.springboot;
 
+import com.java.springboot.dao.GetSC;
+import com.java.springboot.dao.result;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
 
 @SpringBootTest
 class ApplicationTests {
-
+@Autowired
+    GetSC test;
     @Test
     void contextLoads() {
+        List<result>test1=test.resul();
+        test1.forEach(System.out::println);
     }
 
 }
