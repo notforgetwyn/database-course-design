3e58d25e397771c39d287ce48812c13459842016
SHI HUAYUE <1315126403@qq.com>
2024-06-20 15:13:42 +0800

    rewrite Springboot

--- SpringBoot/src/main/java/com/java/springboot/dao/GetSC.java
+++
@@ -1,9 +0,0 @@
-package com.java.springboot.dao;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import java.util.List;
-@Mapper
-public interface GetSC {
-    @Select("SELECT *FROM SC")
-    public List<result> resul();
-}
